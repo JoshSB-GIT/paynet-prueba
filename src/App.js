4c4c6f21e59828c17af78abbc32f0939da186215
Joseph Sierra Beleño <sierrajoseph8023@gmail.com>
2024-06-15 00:13:36 -0500

    for deploy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { HeaderNav } from "./components/HeaderNav/HeaderNav";
 import { Liquidation } from "./components/Liquidation/Liquidation";
 import { MainComponent } from "./components/MainComponent/MainComponent";
 import { GeneralInfo } from "./components/GeneralInfo/GeneralInfo";
 import { PayComponent } from "./components/PayComponent/PayComponent";
+import LoadingScreen from "./components/LoadingScreen/LoadingScreen";
 import "./App.css";
 
 function App() {
   const [generalInfoSubmitted, setGeneralInfoSubmitted] = useState(false);
   const [liquidationSubmitted, setLiquidationSubmitted] = useState(false);
+  const [loading, setLoading] = useState(true); // Estado para controlar la carga
 
   const handleGeneralInfoSubmit = () => {
     setGeneralInfoSubmitted(true);
@@ -18,17 +20,31 @@ function App() {
     setLiquidationSubmitted(true);
   };
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <>
-      <HeaderNav />
-      <MainComponent>
-        <GeneralInfo onFormSubmit={handleGeneralInfoSubmit} />
-        <Liquidation
-          generalInfoSubmitted={generalInfoSubmitted}
-          onFormSubmit={handleLiquidationSubmit}
-        />
-        <PayComponent liquidationSubmitted={liquidationSubmitted} />
-      </MainComponent>
+      {loading ? (
+        <LoadingScreen />
+      ) : (
+        <>
+          <HeaderNav />
+          <MainComponent>
+            <GeneralInfo onFormSubmit={handleGeneralInfoSubmit} />
+            <Liquidation
+              generalInfoSubmitted={generalInfoSubmitted}
+              onFormSubmit={handleLiquidationSubmit}
+            />
+            <PayComponent liquidationSubmitted={liquidationSubmitted} />
+          </MainComponent>
+        </>
+      )}
     </>
   );
 }
