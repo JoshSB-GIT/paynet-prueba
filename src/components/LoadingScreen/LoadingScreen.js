33bbafd46985c6379d53bd169630d05a0aa50241
Joseph Sierra Beleño <sierrajoseph8023@gmail.com>
2024-06-14 23:52:43 -0500

    add loadcream with company logo

---
+++ src/components/LoadingScreen/LoadingScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./LoadingScreen.css";
+
+const LoadingScreen = () => {
+  return (
+    <div className="loading-screen">
+      <div className="cont-logo--load">
+        <div className="logo--load"></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingScreen;
